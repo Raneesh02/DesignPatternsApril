d3e5ae81d4c3e59ec325d396cbbe5440c1a51642
raneeshchoudhary <raneeshchoudhary@gmail.com>
2024-04-20 11:49:15 +0530

    testng dependencies

---
+++ src/test/java/org/example/FirstTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void firstTest(){
+        System.out.println("first test");
+    }
+
+}
